616e7ac30780246855700b63f448ae7605c884af
Bjørn Klinggaard <klinggaard@Bjrn-Klinggaards-MacBook-Pro.local>
2013-08-06 10:02:47 +0200

    version 1.0.1

--- README.md
+++ README.md
@@ -8,4 +8,10 @@ http://dinbror.dk/blazy
 http://dinbror.dk/blog/blazy
 
 ### DESCRIPTION: ###
-bLazy is a lightweight lazy loading image script (only 1KB minified and gzipped). It lets you lazy load and multi-serve your images so you can save bandwidth and server requests. The user will have faster load times and save data loaded if he/she doesn't browse the whole page. 
+bLazy is a lightweight lazy loading image script (less than 1KB minified and gzipped). It lets you lazy load and multi-serve your images so you can save bandwidth and server requests. The user will have faster load times and save data loaded if he/she doesn't browse the whole page. 
+
+## CHANGELOG
+### v 1.0.1 (2013/8/6) ###
+* Performance improvements.
+* Added throttle function to ensure that we don't call resize/scroll functions too often.
+* Cleaning image markup when image has loaded.
--- blazy.js
+++ blazy.js
@@ -3,7 +3,7 @@
  * @type: javascript
  * @author: (c) Bjoern Klinggaard - @bklinggaard
  * @demo: http://dinbror.dk/blazy
- * @version: 1.0.0
+ * @version: 1.0.1
  *==================================================================================================================*/
 ;var Blazy = (function(window, document) {
 	'use strict';
@@ -12,7 +12,12 @@
 	var source;
 	var opt = {}
 	var count = 0;
+	var winHeight;
 	var images = [];
+	
+	//throttle vars
+	var validateT;
+	var saveWinHeightT;
 	
 	// constructor
 	function Blazy(options) {
@@ -26,22 +31,25 @@
 							options.container
 						) 					|| window;
 		source 			= opt.src;
-				
+		//throttle, ensures that we don't call the functions too often
+		validateT		= throttle(validate, 50); 
+		saveWinHeightT	= throttle(saveWinHeight, 50);
+		
+		saveWinHeight();		
 		createImageArray(opt.selector);
 		
-		if(opt.multi) {
-			var multiCount = opt.multi.length;
-			for(var i = 0; i<multiCount; i++){
-				if(opt.multi[i].width >= window.screen.width) {
-					source = opt.multi[i].src;
-					break;
-				}
+		//handle multi-served image src
+		each(opt.multi, function(i){
+			if(opt.multi[i].width >= window.screen.width) {
+				source = opt.multi[i].src;
+				return false;
 			}
-		}
+		});
 		
 		//binding events
-		bindEvent(opt.container, 'scroll', validate);
-		bindEvent(opt.container, 'resize', validate);
+		bindEvent(opt.container, 'scroll', validateT);
+		bindEvent(opt.container, 'resize', validateT);
+		bindEvent(opt.container, 'resize', saveWinHeightT);
 		
 		//start, should blazy ensure domready?
 		validate();		
@@ -56,15 +64,16 @@
 		loadImage(element);
 	};
 	Blazy.prototype.destroy = function(){
-		unbindEvent(opt.container, 'scroll', validate);
-		unbindEvent(opt.container, 'resize', validate);
+		unbindEvent(opt.container, 'scroll', validateT);
+		unbindEvent(opt.container, 'resize', validateT);
+		unbindEvent(opt.container, 'resize', saveWinHeightT);
 		count = 0;
 		images = [];
 	};
 	
 	// private helper functions
 	function validate() {
- 		for(var i = 0; i<count; i++){
+		for(var i = 0; i<count; i++){
  			if(elementInView(images[i])) {
  				loadImage(images[i]);
  				images.splice(i, 1);
@@ -72,6 +81,9 @@
  				i--;
  			} 	
  		}
+		if(count === 0) {
+			Blazy.prototype.destroy();
+		}
 	};
 	
 	function loadImage(ele){
@@ -80,6 +92,11 @@
 		if(ele.offsetWidth > 0 && ele.offsetHeight > 0 && (' ' + ele.className + ' ').indexOf(' loaded ') === -1) {
 			var img = new Image();
 			var src = ele.getAttribute(ele.getAttribute(source) ? source : opt.src);
+			// clean markup, remove data source attributes
+			each(opt.multi, function(i){
+				ele.removeAttribute(opt.multi[i].src);
+			});
+			ele.removeAttribute(opt.src);
 			img.onload = function() {
 		      	!!ele.parent ? ele.parent.replaceChild(img, ele) : ele.src = src;	
 				ele.className = ele.className + ' loaded';	
@@ -91,7 +108,7 @@
 			
 	function elementInView(ele) {
 		var offset = ele.getBoundingClientRect();
-		var bottomline = (window.innerHeight || document.documentElement.clientHeight) + opt.offset;
+		var bottomline = winHeight + opt.offset;
 	    return (
 			offset.top  >= 0
 		 && offset.left >= 0 
@@ -105,7 +122,11 @@
  		var nodelist 	= document.querySelectorAll(selector);
  		count 			= nodelist.length;
  		//converting nodelist to array
- 		for(var i = count; i--; images.unshift(nodelist[i]));
+ 		for(var i = count; i--; images.unshift(nodelist[i])){};
+	 };
+	 
+	 function saveWinHeight(){
+		 winHeight = window.innerHeight || document.documentElement.clientHeight;
 	 };
 	 
 	 function bindEvent(ele, type, fn) {
@@ -123,6 +144,25 @@
          ele.removeEventListener(type, fn, false);
        }
      };
+	 
+	 function each(object, fn){
+ 		if(object && fn) {
+ 			var _count = object.length;
+ 			for(var i = 0; i<_count && fn(i) !== false; i++){}
+ 		}
+	 };
+	 
+	 function throttle(fn, minDelay) {
+     	var lastCall = 0;
+       	return function() {
+        	var now = +new Date();
+         	if (now - lastCall < minDelay) {
+           		return;
+         	}
+         	lastCall = now;
+         	fn.apply(this, arguments);
+       	}
+	 };
   	
 	 return Blazy;
 			  
